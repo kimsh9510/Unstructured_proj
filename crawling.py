dcc6eca3914f9cdc9a8e22bde8059ff29576e2b4
asdf99245 <asdf99245@naver.com>
2022-04-04 20:41:56 +0900

    코드 주석 추가

--- crawling.py
+++ crawling.py
@@ -6,6 +6,7 @@ import time
 
 
 def get_contents_from_news(soup, news):
+    # 뉴스 종류에 따라 기사 내용이 있는 element에서 contents 수집
     selector = None
     if news == 'KBS':
         selector = '#cont_newstext'
@@ -53,23 +54,23 @@ def get_contents_from_news(soup, news):
 
 
 def get_main_contents(driver, news):
-    driver.switch_to.window(driver.window_handles[-1])
+    driver.switch_to.window(driver.window_handles[-1])  # 새 탭으로 이동
     time.sleep(3)
     full_html = driver.page_source
     soup = BeautifulSoup(full_html, "html.parser")
-    contents = get_contents_from_news(soup, news)
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+    contents = get_contents_from_news(soup, news)   # 기사 내용 크롤링
+    driver.close()  # 현재 탭 닫기
+    driver.switch_to.window(driver.window_handles[0])   # 이전 탭으로 이동
     time.sleep(3)
-    return contents.replace('\n', '').replace('\t', '').strip()
+    return contents.replace('\n', '').replace('\t', '').strip()     # 문자열 전처리
 
 
 def get_location(cols):
     ret = ""
     for i in range(0, len(cols)):
-        if cols[i].getText() == 'N/A':
+        if cols[i].getText() == 'N/A':  # 더 이상 정보가 없다면 break
             break
-        if i != 0:
+        if i != 0:  # 첫번째 인덱스가 아니라면 공백 추가
             ret += " "
         ret += cols[i].getText()
 
@@ -88,44 +89,44 @@ def get_data():
     options.add_argument('--blink-settings=imagesEnabled=false')
 
     url = 'http://rscanner.ndmi.go.kr/scanning/damage_news2.php'
-    driver = webdriver.Chrome('chromedriver', options=options)
-    driver.get(url=url)
+    driver = webdriver.Chrome('chromedriver', options=options)  # chrome driver setting with options
+    driver.get(url=url)     # url에 해당하는 사이트를 open
     driver.implicitly_wait(5)
 
     full_html = driver.page_source
     soup = BeautifulSoup(full_html, "html.parser")
     table_body = soup.select_one('.dataTable > tbody')
-    rows = table_body.select('tr')
-    links = driver.find_elements(by=By.CSS_SELECTOR, value="table > tbody > tr > td > a")
+    rows = table_body.select('tr')  # table 각각의 row 배열
+    links = driver.find_elements(by=By.CSS_SELECTOR, value="table > tbody > tr > td > a")   # title 링크 배열
 
     err_cnt = 0
-    filter_type = ["도로교통재난사고", "사업장산재", "안전취약계층사고", "자살"]
+    filter_type = ["도로교통재난사고", "사업장산재", "안전취약계층사고", "자살"]   # filtering type
     result = list()
     for i in range(0, len(rows)):
         try:
             row = rows[i]
             columns = row.select('td')
             disaster_type = columns[1].getText()
-            if disaster_type in filter_type:
+            if disaster_type in filter_type:    # 재난 유형을 filtering
                 continue
-            location = get_location(columns[4:6])
-            date = columns[7].getText()[0:10]
+            location = get_location(columns[4:6])   # location 문자열을 합친다.
+            date = columns[7].getText()[0:10]   # 날짜 뒤의 (None) 제거하여 저장
             news = columns[8].getText()
             title = columns[9].getText()
-            links[i].click()
+            links[i].click()   # 현재 기사의 링크 클릭
             time.sleep(3)
-            contents = get_main_contents(driver, news)
+            contents = get_main_contents(driver, news)     # 기사의 내용을 가져온다.
             line = dict()
             line['type'] = disaster_type
             line['location'] = location
             line['date'] = date
             line['title'] = title
             line['contents'] = contents
-            result.append(line)
+            result.append(line)       # dictionary 생성하여 배열에 삽입
         except Exception as error:
-            print(error)
+            print(error)    # error 발생시 출력하고 err의 개수를 1 증가시킨다.
             err_cnt += 1
 
-    col_news.insert_many(result)
+    col_news.insert_many(result)    # db collection에 크롤링한 데이터 삽입
     print(err_cnt)
     driver.quit()
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-my_client = MongoClient("mongodb://localhost:27017/")
-db = my_client.test
-col_news = db.news
-print(my_client.list_database_names())
+my_client = MongoClient("mongodb://localhost:27017/")   # DB 클라이언트 생성
+db = my_client.test     # 데이터베이스 지정
+col_news = db.news     # 콜렉션 지정
+print(my_client.list_database_names())  # 데이터베이스 리스트 출력
