86e506e29c061a602807544073054c30e9ccae7b
Miso KIM <74090172+miso-art@users.noreply.github.com>
2022-06-06 16:01:41 +0900

    Update Measuring_durartion_disaster.py

--- Measuring_durartion_disaster.py
+++ Measuring_durartion_disaster.py
@@ -51,33 +51,43 @@ class DBHandler:
 
 mongo = DBHandler()
 
-query = {'msg' : {'$regex' : ".*태풍.*"}}
+def average_texting_time(disaster):
+    query = {'msg' : {'$regex' : ".*" + disaster +".*"}}
 
-# i=0
-cnt=0
-denom=0
-prev_date = 0
-prev_location_id=0
+    # i=0
+    cnt=0
+    denom=0
+    prev_date = 0
+    prev_location_id=0
 
-for item in mongo.client["종프1"]["재난문자"].find(query, {"_id": False}, cursor_type=CursorType.EXHAUST).sort('location_id') :
-    # pprint.pprint(item)
-    cnt+=1
-    denom+=1
-    
-    time_data = item['create_date'][0:10]
-    format_data = "%Y/%m/%d"
-    date = datetime.datetime.strptime(time_data, format_data)
+    for item in mongo.client["종프1"]["재난문자"].find(query, {"_id": False}, cursor_type=CursorType.EXHAUST).sort('location_id') :
+        # pprint.pprint(item)
+        cnt+=1
+        denom+=1
+        
+        time_data = item['create_date'][0:10]
+        format_data = "%Y/%m/%d"
+        date = datetime.datetime.strptime(time_data, format_data)
 
-    if item['location_id'] == prev_location_id :
-        if date + datetime.timedelta(days=1) >= prev_date :
-            denom-=1
+        if item['location_id'] == prev_location_id :
+            if date == prev_date :
+                cnt-=1
+                denom-=1
+            elif date + datetime.timedelta(days=1) == prev_date :
+                denom-=1
 
-    prev_date = date
-    prev_location_id = item['location_id']
+        prev_date = date
+        prev_location_id = item['location_id']
 
-    # i+=1
-    # if i == 20 :
-    #     break
+        # i+=1
+        # if i == 20 :
+        #     break
 
-print(cnt, denom, cnt/denom)
-    
+    # print(disaster, cnt, denom, cnt/denom)
+    print(disaster, ":", cnt/denom)
+
+disasters = ["태풍", "홍수", "가뭄", "강풍", "해일","산사태", "대설", "한파", "폭염", "지진",
+"황사", "화재", "산불", "붕괴", "교통사고", "미세먼지", "감염병", "코로나", "확진"]
+
+for disaster in disasters : 
+    average_texting_time(disaster)
